5b0a16214fcb01062e004d4a649f6b786a2a539f
Kamil Gargula <kamil.gargula@student.pk.edu.pl>
2026-01-14 00:53:09 +0000

    feat: test integration completed  - setup Catch2  - make CmakeLists test: SharedMemory  - write test to proof Catch2 work fix: SharedMemory  - fix shmat priviliges  - improve logging  - add owner logic to secure it more  - fix minor errors

---
+++ test/SharedMemoryTest.cpp
@@ -0,0 +1,28 @@
+#include "catch2/catch_test_macros.hpp"
+#include "SharedMemory.hpp"
+
+#include "utils.hpp"
+
+
+TEST_CASE("SharedMemory create/assign and share data ", "[sharedmemory]") {
+    logging::init();
+    SharedMemory shm{true};
+
+    //assign sth
+    auto *p = shm.getPtr();
+    REQUIRE(p != nullptr);
+    auto data = DataSOR();
+    data.isOpen = true;
+    data.lekarzePIDs[0] = 1234;
+
+    //commit it
+    *p = data;
+
+    SharedMemory shm2;
+    auto *p2 = shm2.getPtr();
+
+    //read test
+    REQUIRE(p2 != nullptr);
+    REQUIRE(p2->isOpen == true);
+    REQUIRE(p2->lekarzePIDs[0] == 1234);
+}
